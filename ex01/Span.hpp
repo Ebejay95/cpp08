7777945c759e1e2932f2e8f2d9e8b1adea5251a1
Jonathan Eberle <jonathan.eberle@gmx.de>
2025-03-22 09:36:05 +0100

    span finished

--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -1,22 +1,38 @@
 #ifndef SPAN_HPP
 # define SPAN_HPP
 
-	#include <vector>
+#include <vector>
+#include <stdexcept>
+#include <algorithm>
 
-	class Span {
-		private:
-			unsigned int n;
-			std::vector<int> numbers;
-		public:
-			Span();
-			Span(unsigned int initer);
-			Span(const Span& other);
-			Span& operator=(const Span& other);
-			~Span();
+class Span {
+    private:
+        unsigned int n;
+        std::vector<int> numbers;
+    public:
+        Span();
+        Span(unsigned int initer);
+        Span(const Span& other);
+        Span& operator=(const Span& other);
+        ~Span();
 
-			void addNumber(int number);
-			unsigned int shortestSpan(void);
-			unsigned int longestSpan(void);
-	};
+        void addNumber(int number);
+        unsigned int shortestSpan(void);
+        unsigned int longestSpan(void);
+
+        // Add a range of numbers using iterators
+        template <typename Iterator>
+        void addRange(Iterator begin, Iterator end);
+};
+
+template <typename Iterator>
+void Span::addRange(Iterator begin, Iterator end) {
+    while (begin != end) {
+        if (numbers.size() >= n)
+            throw std::runtime_error("Span is full");
+        numbers.push_back(*begin);
+        ++begin;
+    }
+}
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,10 +1,55 @@
 #include "Span.hpp"
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
 
 int main() {
-	Span* spanner = new Span(6);
-	Span* freaker;
-	freaker = spanner;
-	delete spanner;
-	delete freaker;
-	return 0;
+    try {
+        Span sp = Span(5);
+        sp.addNumber(6);
+        sp.addNumber(3);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
+
+        std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+        std::cout << "Longest span: " << sp.longestSpan() << std::endl;
+
+        Span largeSpan(10000);
+        std::vector<int> numbers;
+
+        srand(time(NULL));
+        for (int i = 0; i < 10000; ++i) {
+            numbers.push_back(rand());
+        }
+
+        largeSpan.addRange(numbers.begin(), numbers.end());
+
+        std::cout << "Large span - Shortest: " << largeSpan.shortestSpan() << std::endl;
+        std::cout << "Large span - Longest: " << largeSpan.longestSpan() << std::endl;
+
+        Span emptySpan(5);
+        try {
+            std::cout << "Testing empty span: ";
+            emptySpan.shortestSpan();
+        } catch (const std::exception& e) {
+            std::cout << "Exception: " << e.what() << std::endl;
+        }
+
+        Span fullSpan(2);
+        fullSpan.addNumber(1);
+        fullSpan.addNumber(2);
+        try {
+            std::cout << "Testing full span: ";
+            fullSpan.addNumber(3);
+        } catch (const std::exception& e) {
+            std::cout << "Exception: " << e.what() << std::endl;
+        }
+
+    } catch (const std::exception& e) {
+        std::cout << "Unhandled exception: " << e.what() << std::endl;
+    }
+
+    return 0;
 }
